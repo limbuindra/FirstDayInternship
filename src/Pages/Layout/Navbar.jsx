7341f62011e74d58d7adfceb693f4d7610bb5edb
indra limbu <indralimbu324@gmail.com>
2025-01-23 20:14:50 +0545

    Navbar

--- src/Pages/Layout/Navbar.jsx
+++ src/Pages/Layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaStar } from "react-icons/fa6";
 
 const Navbar = () => {
 	const [isMenuOpen, setMenuOpen] = useState(false);
@@ -11,24 +12,25 @@ const Navbar = () => {
 
 	return (
 		<>
-			<nav className="">
-				<div className="mx-auto max-w-7xl  px-6 lg:px-8 flex justify-end text-xl lg:p-4" >
-					<div className="relative flex h-16 items-center flex-end ">
-						<div className="absolute inset-y-0 left-0 justify-center flex  items-center sm:hidden">
+			<nav >
+					<div className="relative flex h-20  lg:flex ">
+						<div className="absolute inset-y-0 left-0 justify-center flex  items-center lg:hidden">
 							<button
 								onClick={toggleMenu}
 								type="button"
-								className="inline-flex  justify-center rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+								className="   text-gray-500 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 							>
 								<GiHamburgerMenu />
 							</button>
 						</div>
 						
 
-						<div className=" hidden sm:flex sm:ml-6 items-center justify-end space-x-4">
-						<div className="text-4xl ">
-							<h1 >	Positivus</h1>
+						<div className=" hidden lg:flex ml-6 lg:text-xl lg:w-full lg:items-center  lg:justify-evenly  ">
+						<div className="text-5xl lg:flex">
+							<FaStar/>	<h1>Positivus</h1>
 						</div>
+						<div className="lg:flex lg:justify-center lg:items-center lg:space-x-4">
+
 							<ul className="flex space-x-8">
 								<li>
 									<Link
@@ -50,7 +52,7 @@ const Navbar = () => {
 									<Link
 										to="/usecase"
 										className="text-gray-700 hover:text-gray-900"
-									>
+										>
 										Use Cases
 									</Link>
 								</li>
@@ -74,12 +76,13 @@ const Navbar = () => {
 						<div className="border p-4 rounded-2xl">
 							<button>Request from quote</button>
 						</div>
+										</div>
 						</div>
 					</div>
-				</div>
+				{/* </div> */}
 
 				<div
-					className={`sm:hidden fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform ${
+					className={`lg:hidden fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform ${
 						isMenuOpen ? "translate-x-0" : "-translate-x-full"
 					} transition-transform duration-300 ease-in-out`}
 				>
